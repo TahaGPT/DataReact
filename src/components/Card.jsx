1ceb8ac3daf1bb5f2bf5379a8dc37097dcb9b28c
TahaGPT <tahaaahmed12115@gmail.com>
2025-06-04 01:25:02 +0500

    Hard Coded Journal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,34 @@
-import korean1 from "../assets/korean1.jpg"
+import markerImage from "../assets/marker.png"
 
-export default function Card() {
-    return (
-      <article>
-        <div className="card box">
-          <img src={korean1} alt="Korean Nigga" />
-        </div>
-        <h1>Kim Seok Jin</h1>
-        <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi optio
-        blanditiis porro necessitatibus ducimus sed maiores quibusdam
-        soluta, repudiandae accusantium ad quis ex temporibus dicta, maxime
-        reiciendis qui? Dignissimos, recusandae!
+export default function Entry(props) {
+  console.log(props)
+  return (
+    <article className="journal-entry">
+      <div className="main-image-container">
+        <img
+          className="main-image"
+          src="https://scrimba.com/links/travel-journal-japan-image-url"
+          alt="mount fuji"
+        />
+      </div>
+      <div className="info-container">
+        <img
+          className="marker"
+          src= {markerImage}
+          alt="map marker icon"
+        />
+        <span className="country">Japan</span>
+        <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
+          View on Google Maps
+        </a>
+        <h2 className="entry-title">Mount Fuji</h2>
+        <p className="trip-dates">12 Jan, 2021 - 24 Jan, 2021</p>
+        <p className="entry-text">
+          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
+          (12,380 feet). Mount Fuji is the single most popular tourist site in
+          Japan, for both Japanese and foreign tourists.
         </p>
-      </article>
-    );
+      </div>
+    </article>
+  );
 }
