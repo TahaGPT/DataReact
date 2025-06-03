c07c95b88b3fc5ce4974f5cccb6080804c5acd9a
TahaGPT <tahaaahmed12115@gmail.com>
2025-06-04 01:38:25 +0500

    Props Implemented

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -16,7 +16,7 @@ export default function MainComponent() {
         <Card
           img={{
             src: "https://scrimba.com/links/travel-journal-japan-image-url",
-            alt: "Mount Fuji",
+            alt: "Mount Fuji"
           }}
           title="Mount Fuji"
           country="Japan"
@@ -24,9 +24,9 @@ export default function MainComponent() {
           dates="12 Jan, 2021 - 24 Jan, 2021"    
           text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
         />
+        {/* <Card />
         <Card />
-        <Card />
-        <Card />
+        <Card /> */}
       </section>
     </main>
   );
